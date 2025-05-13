9da7fa2ce9c4a41da11c4487e5c24d335a0c6cbf
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2025-05-13 19:02:03 +0700

    rv2 tabel users

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,16 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+// api/routes/index.js
 
-const routesDir = __dirname;
+const authRoutes = require('./auth');
+const profileRoutes = require('./profile');
+// Tambahkan file route lain di sini jika ada
 
-// Baca semua file .js kecuali index.js
-const routeFiles = fs.readdirSync(routesDir)
-  .filter(file => file !== 'index.js' && file.endsWith('.js'));
-
-// Gabungkan semua route dari tiap file
-const allRoutes = routeFiles.flatMap(file => {
-  const route = require(path.join(routesDir, file));
-  return Array.isArray(route) ? route : [route]; // handle single/multiple routes
-});
-
-module.exports = allRoutes;
+module.exports = [
+  ...authRoutes,
+  ...profileRoutes,
+];
